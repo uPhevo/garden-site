fd89851bb4306de4bcfd4aec4f26f88456eb3e74
James Carpenter <elklemente777@gmail.com>
2025-09-06 20:26:50 +0700

    Добавил новую почту

--- garden_site/settings.py
+++ garden_site/settings.py
@@ -45,9 +45,9 @@ SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 # Email
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = os.getenv("EMAIL_HOST", "mail.timeweb.com")
-EMAIL_PORT = int(os.getenv("EMAIL_PORT", 465))
-EMAIL_USE_SSL = str(os.getenv("EMAIL_USE_SSL", "True")).strip().lower() in ("1", "true", "yes")
-EMAIL_USE_TLS = str(os.getenv("EMAIL_USE_TLS", "False")).strip().lower() in ("1", "true", "yes")
+EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
+EMAIL_USE_SSL = str(os.getenv("EMAIL_USE_SSL", "False")).strip().lower() in ("1", "true", "yes")
+EMAIL_USE_TLS = str(os.getenv("EMAIL_USE_TLS", "True")).strip().lower() in ("1", "true", "yes")
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "info@сказочныйсад.рф")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "password")
 DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", EMAIL_HOST_USER)
