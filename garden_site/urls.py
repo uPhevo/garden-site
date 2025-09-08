2030ff5bce51368d471f51bb241137ac3d5a5ec6
James Carpenter <elklemente777@gmail.com>
2025-09-08 19:37:20 +0700

    Some fixes

--- garden_site/urls.py
+++ garden_site/urls.py
@@ -4,17 +4,27 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.sitemaps.views import sitemap
 from .sitemaps import StaticViewSitemap, FlowerSitemap 
+from django.http import HttpResponse
 
 sitemaps = {
     'static': StaticViewSitemap(),
     'flowers': FlowerSitemap(),
 }
 
+def robots_txt(request):
+    content = """User-agent: *
+Disallow:
+
+Sitemap: http://xn--80aamkkdxhxg7d7b.xn--p1ai/sitemap.xml
+"""
+    return HttpResponse(content, content_type="text/plain")
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='sitemap'),
+    path("robots.txt", robots_txt),
 ]
 
 if settings.DEBUG:
